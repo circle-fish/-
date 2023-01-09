dc3706c87d121b77f77ca0e32d257f02a7912053
circle-fish <583558178@qq.com>
2023-01-09 16:22:54 +0800

    “粒子系统”

---
+++ Assets/Scenes/particleRing.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particleRing : MonoBehaviour
+{
+    public ParticleSystem ps;
+    int directionx,directiony;
+
+    void Start() {
+        ps= GetComponent<ParticleSystem>();
+        directionx = 0;
+        directiony = 0;
+    }
+ 
+    void Update() {
+        int minX = -10, maxX = 10 ;
+        if (directionx == 0 && ps.transform.position.x > minX){
+            ps.transform.position += Vector3.left * Time.deltaTime * 50;
+            Vector3 tmp=ps.transform.position;
+            double numy=100.0f-(ps.transform.position.x*ps.transform.position.x);
+            if(numy<0) numy=0;
+            else numy=Math.Sqrt(numy);
+            if(directiony==0) numy=numy*-1;
+            tmp.y=(float)numy;
+            ps.transform.position=tmp;
+        } else if (directionx == 1 && ps.transform.position.x < maxX){
+            ps.transform.position += Vector3.right * Time.deltaTime * 50;
+            Vector3 tmp=ps.transform.position;
+            double numy=100.0f-(ps.transform.position.x*ps.transform.position.x);
+            if(numy<0) numy=0;
+            else numy=Math.Sqrt(numy);
+            if(directiony==0) numy=numy*-1;
+            tmp.y=(float)numy;
+            ps.transform.position=tmp;
+        } else{
+            directionx=1-directionx;
+            directiony=1-directiony;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+
+
+
+想看效果直接把我的Assets替换，然后使用MyScene即可。
+相关博客地址：  
+可访问我的博客：[粒子系统](https://blog.csdn.net/weixin_43912333/article/details/110425220)  
+
+参考博客：(https://blog.csdn.net/toupiOfRivia/article/details/70768406?ops_request_misc=%257B%2522request%255Fid%2522%253A%2522167324740916800225537175%2522%252C%2522scm%2522%253A%252220140713.130102334..%2522%257D&request_id=167324740916800225537175&biz_id=0&utm_medium=distribute.pc_search_result.none-task-blog-2~all~baidu_landing_v2~default-2-70768406-null-null.142^v70^pc_new_rank,201^v4^add_ask&utm_term=unity%E5%88%B6%E4%BD%9C%E7%A5%9E%E5%A5%87%E7%9A%84%E7%B2%92%E5%AD%90%E6%B5%B7%E6%B4%8B&spm=1018.2226.3001.4187
+https://blog.csdn.net/qq_36297981/article/details/80495404?spm=1001.2101.3001.6650.15&utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7ERate-15-80495404-blog-71055766.pc_relevant_default&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7ERate-15-80495404-blog-71055766.pc_relevant_default&utm_relevant_index=24
+)
